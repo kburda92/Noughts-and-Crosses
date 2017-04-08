370574fb40f114698453e340fe713c99160e71a8
krzysiak <krzysiak1992@gmail.com>
2017-04-08 23:55:20 +0200

    template method for array

--- Utils.h
+++ Utils.h
@@ -1,12 +1,13 @@
 #ifndef UTILS_H
 #define UTILS_H
+#include <array>
 
-enum class Figure
+//template method checking does array contains desired value
+template<std::size_t size>
+bool ContainsValue(std::array<int, size>& tab, int desired_value)
 {
-    empty,
-    nought,
-    cross
-};
+    return std::any_of(begin(tab), end(tab),[desired_value](int value) {return value == desired_value;});
+}
 
 
 
